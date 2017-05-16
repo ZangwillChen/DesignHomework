ab2847745faa2452037caab6528aad1ab26b8b93
ZangwillChen <954162217@qq.com>
2017-05-16 18:59:01 +0800

    用户类型实体类

--- src/com/czw/entity/Role.java
+++ src/com/czw/entity/Role.java
@@ -1,7 +1,21 @@
 package com.czw.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by chenzhaowen on 2017/5/16.
  */
 public class Role {
+
+    private long roleID;
+    private String roleName;
+    private int permission;
+    private Set<User> users = new HashSet<User>();
+
+    public Role{
+
+    }
+
+    public
 }
