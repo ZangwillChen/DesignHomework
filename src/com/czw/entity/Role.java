3a9067f57684a6064f3d05a74e5521ae86c46654
ZangwillChen <954162217@qq.com>
2017-05-16 19:08:33 +0800

    用户类型实体类

--- src/com/czw/entity/Role.java
+++ src/com/czw/entity/Role.java
@@ -11,11 +11,44 @@ public class Role {
     private long roleID;
     private String roleName;
     private int permission;
-    private Set<User> users = new HashSet<User>();
+    private Set<User> users = new HashSet<>();
 
-    public Role{
-
+    public Role(long roleID,String roleName,int permission,Set<User> users){
+        this.roleID = roleID;
+        this.roleName = roleName;
+        this.permission = permission;
+        this.users = users;
     }
 
-    public
+    public long getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(long roleID) {
+        this.roleID = roleID;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public int getPermission() {
+        return permission;
+    }
+
+    public void setPermission(int permission) {
+        this.permission = permission;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
 }
