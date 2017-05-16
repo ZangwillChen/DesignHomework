e408e63fdc79863003376f23fca9268e59f47c29
ZangwillChen <954162217@qq.com>
2017-05-16 17:39:53 +0800

    a test model

--- src/com/czw/entity/User.java
+++ src/com/czw/entity/User.java
@@ -15,11 +15,20 @@ public class User {
     private String userPhone;
     private String userEmail;
     private String ReserveInf;
-    Set<Role> userType;
+    private Role userType;
 
-    public User(){
+    public  User(){
 
     }
 
-    public User(long userID,String userName,String userPassword,)
+    public User(long userID,String userName,String userPassword,
+                String userPhone,String userEmail,String ReserveInf,Role userType){
+        this.userID = userID;
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.userPhone = userPhone;
+        this.userEmail = userEmail;
+        this.ReserveInf = ReserveInf;
+        this.userType = userType;
+    }
 }
