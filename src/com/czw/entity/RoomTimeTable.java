a4bfd175b5f76154df354f8bef7f565a671e9549
ZangwillChen <954162217@qq.com>
2017-05-17 20:33:01 +0800

    房间时间表实体类

---
+++ src/com/czw/entity/RoomTimeTable.java
@@ -0,0 +1,32 @@
+package com.czw.entity;
+
+/**
+ * Created by chenzhaowen on 2017/5/17.
+ * @brief 房间时间表实体类
+ */
+public class RoomTimeTable {
+
+    private Room room;
+    private String roomStatus;
+
+    public RoomTimeTable(Room room,String roomStatus){
+        this.room = room;
+        this.roomStatus = roomStatus;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public String getRoomStatus() {
+        return roomStatus;
+    }
+
+    public void setRoomStatus(String roomStatus) {
+        this.roomStatus = roomStatus;
+    }
+}
