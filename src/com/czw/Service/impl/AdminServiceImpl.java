d5aa5f1494126f175913790cc008e7b64eb76d66
ZangwillChen <954162217@qq.com>
2017-05-23 16:30:58 +0800

    changed in 05231623

--- src/com/czw/Service/impl/AdminServiceImpl.java
+++ src/com/czw/Service/impl/AdminServiceImpl.java
@@ -1,7 +1,20 @@
 package com.czw.Service.impl;
 
+import com.czw.Dao.impl.BaseDaoImpl;
+import com.czw.Service.AdminService;
+import com.czw.entity.Admin;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
 /**
  * Created by chenzhaowen on 2017/5/23.
  */
-public class AdminServiceImpl {
+@Service
+@Transactional
+public class AdminServiceImpl extends BaseDaoImpl<Admin> implements AdminService {
+
+    Admin admin;
+    @Override
+
 }
