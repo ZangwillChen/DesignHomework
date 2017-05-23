e4327f0544a92ac37bcd36ebcf6f1604a1fb44eb
ZangwillChen <954162217@qq.com>
2017-05-23 19:37:47 +0800

    updated in 05231937

--- src/com/czw/Service/UserService.java
+++ src/com/czw/Service/UserService.java
@@ -7,19 +7,50 @@ import java.util.List;
 
 /**
  * Created by chenzhaowen on 2017/5/23.
+ * @brief 用户业务逻辑实体类
  */
 public interface UserService extends BaseDao<User> {
 
+    /*
+    * @brief 获取所有用户逻辑接口
+    * @return
+    * */
     List<User> userFindAll();
 
+    /*
+    * @brief 根据姓名获取用户信息逻辑接口
+    * @param userName
+    * @return
+    * */
     List<User> getUserByName(String userName);
 
+    /*
+    * @brief 用户登录逻辑接口
+    * @param userName
+    * @param userPassword
+    * @return
+    * */
     User user_login(String userName,String userPassword);
 
+    /*
+    * @brief 添加用户逻辑接口
+    * @param user
+    * @return
+    * */
     User userAdd(User user);
 
+    /*
+    * @brief 根据ID删除用户逻辑接口
+    * @param userID
+    * @return
+    * */
     User userDeleteById(long userID);
 
+    /*
+    * @brief 更新用户信息逻辑接口
+    * @param user
+    * @return
+    * */
     User updateUserInfo(User user);
 
 }
